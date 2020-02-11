34d8deb32de1d484074c9cf51f7cd23b0ffe5b42
Ian Grubb <hi@iangrubb.com>
2020-02-11 13:52:43 -0500

    pre lecture

---
+++ 27-big-O/index.js
@@ -0,0 +1,175 @@
+
+
+const reverseString = string => {
+
+    // "matt" => "ttam"
+    // 
+    // "loves" => "sevol"
+    //
+    // “vaporwave” => “evawropav”
+
+    let result = ""
+
+    for (let i = 0 ; i < string.length ; i++) {
+        result = string[i] + result
+    }
+
+    return result
+
+}
+
+const isPalindrome = string => {
+
+    // "vaporwave" => false
+    // "civic" => true
+
+    let result = true
+
+    const requiredLength = Math.floor( string.length / 2 )
+
+    for (let i = 0 ; i < requiredLength ; i++) {
+
+        console.log(i, string[i])
+        console.log(string.length - 1 - i, string[string.length - 1 - i])
+
+        if (string[i] !== string[string.length - 1 - i]) {
+            console.log("Counterexample")
+            result = false
+        }
+
+    }
+
+    return result
+
+}
+
+const findChar = (string, target) => {
+
+    // ("vaporwave", "o") => 3
+    // ("vaporwave", "s") => null
+
+    let result = null
+    let i = 0
+
+    while (!result && i < string.length) {
+
+        console.log(i)
+
+        if (string[i] === target) {
+            result = string[i]
+        }
+
+
+        i++
+    }
+
+    return result
+
+}
+
+
+
+
+
+
+const linearSearch = (arr, target) => {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(`Remaining elements to search: ${arr.length - i}`);
+  
+      const elem = arr[i];
+      if (elem === target) {
+        return target;
+      }
+    }
+    return false;
+  };
+
+
+
+
+
+
+
+const binarySearch = (arr, target) => {
+    console.log(`Remaining elements to search: ${arr.length}`);
+  
+    const midpoint = Math.floor(arr.length / 2);
+    const middle = arr[midpoint];
+  
+    if (arr.length <= 1) {
+      return middle === target ? target : false;
+    }
+  
+    if (middle === target) {
+      // we found the thing
+      return target
+    } else if (middle >= target) {
+      // let's look in the first half
+      return binarySearch(arr.slice(0, midpoint), target);
+    } else if (middle < target){
+      // let's look in the second half
+      return binarySearch(arr.slice(midpoint), target);
+    }
+  };
+
+
+
+
+
+
+  const repeatsLetters = string => {
+
+    // "vaporwave" => true
+    // "grape" => false
+
+    let result = false
+
+    for (let i = 0 ; i < string.length ; i++) {
+
+        const firstLetter = string[i]
+
+        for (let j = i + 1 ; j < string.length ; j++) {
+
+            const secondLetter = string[j]
+
+            console.log(i, j)
+
+            if (firstLetter === secondLetter) {
+                result = true
+            }
+        }
+    }
+
+    return result
+
+
+  } 
+
+
+
+  const repeatsLettersQuickly = string => {
+
+    let result = false
+
+    const hash = {}
+
+    for (let i = 0 ; i < string.length ; i++) {
+
+        const char = string[i]
+
+        console.log(char)
+        console.log(hash)
+        console.log(result)
+
+        if (hash[char]) {
+            result = true
+        }
+
+        hash[char] = true
+
+    }
+
+    return result
+
+
+  }
---
+++ 27-big-O/readme.md
@@ -0,0 +1,65 @@
+
+# **Algorithms and Big O Notation**
+
+## SWBATs
+- [ ] Understand what algorithms are
+- [ ] Understand why we use Big O notation to measure algorithmic efficiency
+- [ ] Identify common runtimes ( constant, logarithmic, linear, quasi-linear, quadratic )
+- [ ] Assess time / space tradeoffs
+
+## What are algorithms?
+
+
+
+
+## How do we measure the efficiency of an algorithm?
+
+Why not just time how long it takes an algorithm to run?
+
+
+
+
+
+Big picture facts about Big O:
+
+1. A high-level, approximate measure of complexity.
+2. Measure of the relationship between the size of the input to the algorithm and the work required to run the algorithm.
+    - n is a variable that represents the size of the input
+    - O is a function from that variable to an ammount of work
+    - What matters: how much more work is required if we increse the input size by 1?
+3. Used to measure both time complexity and space complexity.
+
+
+
+## Common Big O Increments 
+
+![Big O measures ](https://nedbatchelder.com/text/bigo_pix/011.png)
+
+1. Constant Time        O(1)
+
+    -
+
+2. Logarithmic Time     O(log(n))
+
+    -
+
+3. Linear Time          O(n)
+
+    -
+
+4. Quasi-linear Time    O(n * log(n))
+
+    -
+
+5. Quadratic            O(n^2)
+
+    -
+
+
+## Guidelines for measuring Big O 
+
+1.
+2. 
+3. 
+4. 
+5. 
