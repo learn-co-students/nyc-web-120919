ac2d8171dd74ea994c31289dd15c9bbc8370ad63
Caryn McCarthy <cmccarthy15@gmail.com>
2020-02-19 09:12:00 -0500

    state events and forms starter

--- 31-state-events/silkmeme/src/App.js
+++ 31-state-events/silkmeme/src/App.js
@@ -5,11 +5,43 @@ import SearchBar from './SearchBar';
 import MainContainer from './MainContainer';
 
 class App extends React.Component {
+
+  renderNavBar = () => {
+    return (
+      <div className="NavBar">
+          <div id="silk-meme-icon">
+            <img className="icon-photo" alt="silk" src="https://heiq.com/wp-content/uploads/2016/10/shutterstock_329722193.jpg"/>
+            <span className="icon-title">SilkMeme</span>
+          </div>
+          <button className="nav option">Featured</button>
+          <button id="subreddit-1" className="nav option">Memes</button>
+          <button id="subreddit-2" className="nav option">DankMemes</button>
+          <button id="subreddit-3" className="nav option">MeIRL</button>
+          <button className="nav option upload">Upload</button>
+          <button className="nav option login">Login</button>
+        </div>
+    )
+  }
+
+  renderUploadForm = () => {
+    return (
+      <div className="upload-form">
+        <form>
+          <h3>Upload a new Silky Meme</h3>
+            <input placeholder="Title"/>
+            <input placeholder="Image URL"/>
+            <button type="submit">Submit</button>
+        </form>
+
+      </div>
+    )
+  }
   
   render() {
     return (
       <div className="App">
-        <NavBar />
+        {this.renderNavBar()}
+        {this.renderUploadForm()}
         <SearchBar />
         <MainContainer />
       </div>
@@ -18,13 +50,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-// const myFavFunc = () => {
-//   console.log('hay faves')
-// }
-
-// export { App, myFavFunc };
-
-// import { App, myFavFunc } from './App';
-
